96cad6d32bcb41ba8d57f10225673cb9c39cd6fc
Gašper Novak <gaspernovak@proton.me>
2023-03-15 11:48:16 +0100

    Prewitt edge detection
    Added function for Prewitt edge detection

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import cv2 as cv
 import numpy as np
 
 def my_roberts(image):
-    kernel = np.array([[1.0, 0.0],  #robert's cross
+    kernel = np.array([[1.0, 0.0],  #roberts cross
                        [0.0, -1.0]])
     kernel = kernel/(np.sum(kernel) if np.sum(kernel)!=0 else 1)
     edge_image = cv.filter2D(image, -1, kernel)
     return edge_image
 
 def my_prewitt(image):
-    pass
+    kernel = np.array([[1.0, 0.0, -1.0],
+                       [1.0, 0.0, -1.0],
+                       [1.0, 0.0, -1.0]])
+    
+    kernel = kernel/(np.sum(kernel) if np.sum(kernel) != 0 else 1)
+    kernel_t = np.transpose(kernel)
+    
+    image = cv.filter2D(image, -1, kernel)
+    edge_image = cv.filter2D(image, -1, kernel_t)
+    
     return edge_image
 
 def my_sobel(image):
@@ -32,4 +41,7 @@ image_contrast = spremeni_kontrast(image, 3, -80)
 cv.imwrite("./images/image_contrast.jpg", image_contrast)
 
 image_roberts = my_roberts(image)
-cv.imwrite("./images/image_robers.jpg", image_roberts)
+cv.imwrite("./images/image_roberts.jpg", image_roberts)
+
+image_prewitt = my_prewitt(image)
+cv.imwrite("./images/image_prewitt.jpg", image_prewitt)
