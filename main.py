83664bed037a806f8c5cbe806ef40326f84db8fc
Gašper Novak <gaspernovak@proton.me>
2023-03-15 13:09:42 +0100

    Canny edge detection
    Added function for Canny edge detection

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def my_sobel(image):
     kernel = np.array([[1.0, 0.0, -1.0],
                        [2.0, 0.0, -2.0],
                        [1.0, 0.0, -1.0]])
+    
     kernel = kernel/(np.sum(kernel) if np.sum(kernel) != 0 else 1)
     kernel_t = np.transpose(kernel)
     
@@ -32,8 +33,9 @@ def my_sobel(image):
     edge_image = cv.filter2D(image, -1, kernel_t)
     return edge_image
 
-def canny(slika, bottom, top):
-    pass
+# takes in sobel output
+def my_canny(image, threshold_bottom=0, threshold_top=0):
+    edge_image = cv.Canny(image, threshold_bottom, threshold_top)
     return edge_image
 
 def spremeni_kontrast(image, alpha, beta):
@@ -43,6 +45,7 @@ def spremeni_kontrast(image, alpha, beta):
 
 image = cv.imread("./images/image.jpg")
 image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+image = cv.GaussianBlur(image, (5, 5), 5)
 
 image_contrast = spremeni_kontrast(image, 3, -80)
 cv.imwrite("./images/image_contrast.jpg", image_contrast)
@@ -55,3 +58,6 @@ cv.imwrite("./images/image_prewitt.jpg", image_prewitt)
 
 image_sobel = my_sobel(image)
 cv.imwrite("./images/image_sobel.jpg", image_sobel)
+
+image_canny = my_canny(image, 255, 30)
+cv.imwrite("./images/image_canny.jpg", image_canny)
