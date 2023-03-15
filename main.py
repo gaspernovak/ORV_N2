f8964f26836789f155bc0e288fe8ebd491dbd975
Gašper Novak <gaspernovak@proton.me>
2023-03-15 11:30:37 +0100

    Roberts edge detection
    Added function for roberts edge detection

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import cv2 as cv
 import numpy as np
 
-def my_robers(image):
-    pass
+def my_roberts(image):
+    kernel = np.array([[1.0, 0.0],  #robert's cross
+                       [0.0, -1.0]])
+    kernel = kernel/(np.sum(kernel) if np.sum(kernel)!=0 else 1)
+    edge_image = cv.filter2D(image, -1, kernel)
     return edge_image
 
 def my_prewitt(image):
@@ -17,16 +20,16 @@ def canny(slika, bottom, top):
     pass
     return edge_image
 
+def spremeni_kontrast(image, alpha, beta):
+    image = cv.multiply(image, alpha)
+    image = cv.add(image, beta)
+    return image
 
-cv.namedWindow("Image")
 image = cv.imread("./images/image.jpg")
 image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
-kernel = np.array([1, 1, 1], 
-                  [1, 1, 1],
-                  [1, 1, 1])
+image_contrast = spremeni_kontrast(image, 3, -80)
+cv.imwrite("./images/image_contrast.jpg", image_contrast)
 
-print(kernel)
-
-cv.imshow("Image", image)
-cv.waitKey(0)
+image_roberts = my_roberts(image)
+cv.imwrite("./images/image_robers.jpg", image_roberts)
